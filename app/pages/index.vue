9689b579f3326e1ee3b340027401fef7275d593a
Takayuki Ueno <mail@kichi2004.jp>
2020-08-11 22:56:55 +0900

    管理ページへのリンク追加

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,6 +19,7 @@
           </tr>
         </table>
       </v-simple-table>
+      <nuxt-link v-if="loggedIn" to="manage">管理ページへ</nuxt-link>
     </v-card-text>
   </v-card>
 </template>
@@ -27,12 +28,17 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 import dayjs from 'dayjs'
 import { Contest } from '~/types/contest'
+import { userStore } from '~/utils/store-accessor'
 
 @Component
 export default class PageMainPage extends Vue {
   contests: Contest[] | null = null
   async created() {
     this.contests = await this.$api.Contests.index()
+  }
+
+  get loggedIn() {
+    return userStore.getUser && userStore.getUser.role !== 'member'
   }
 
   formatDate = (date: string) => dayjs(date).format('MM/DD HH:mm')
